a6eae5c3d5d557e2153e55a33d309d61098b50a2
Mat Ludlam <matludlam@hotmail.com>
2025-12-03 08:37:59 +0000

    Solution to day 3 of Advent of Code 2025.

--- src/main/java/day03/Day03.java
+++ src/main/java/day03/Day03.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * To solve https://adventofcode.com/2025/day/XX
@@ -16,8 +19,8 @@ import java.util.Objects;
  * 
  * @author mat
  * 
- *         Part 1: X
- *         Part 2: X
+ *         Part 1: 17159
+ *         Part 2: 170449335646486
  * 
  * 
  */
@@ -31,6 +34,7 @@ public class Day03 {
     private static final String DISPLAY_TEXT = "The solution to part %d is %s and took %d miliseconds%n";
 
     // Variables for this problem
+    List<int[]> batteryList = new ArrayList<>();
 
     /**
      * *****************************************************************************
@@ -40,7 +44,10 @@ public class Day03 {
      */
     public Day03(List<String> inputLines) {
         // Parse input here
-        inputLines.forEach(line -> System.out.println("Input Line: " + line));
+        inputLines.forEach(line -> batteryList.add(
+                line.chars()
+                        .map(c -> c - '0')
+                        .toArray()));
     }
 
     /**
@@ -50,8 +57,37 @@ public class Day03 {
      * @return solution to part 1
      */
     public String solve1() {
-        // Implement solution for part 1 here
-        return "solve1";
+        long total = 0;
+
+        for (int[] battery : batteryList) {
+            int value1 = 0;
+            int position1 = 0;
+            int value2 = 0;
+
+            // Get 1st digit, stop 1 before the end
+            for (int cell = 0; cell < battery.length - 1; cell++) {
+                if (battery[cell] > value1) {
+                    value1 = battery[cell];
+                    position1 = cell;
+                }
+            }
+
+            // Get the 2nd digit, start 1 after the 1st digit
+            for (int cell = position1 + 1; cell < battery.length; cell++) {
+                if (battery[cell] > value2) {
+                    value2 = battery[cell];
+                }
+            }
+
+            int result = value1 * 10 + value2;
+            total += result;
+
+            if (DEBUG) {
+                System.out.printf("For battery: %s, max values are %d%n", intArrayToString(battery), result);
+            }
+        }
+
+        return Long.toString(total);
     }
 
     /**
@@ -61,14 +97,59 @@ public class Day03 {
      * @return
      */
     public String solve2() {
-        // Implement solution for part 2 here
-        return "solve2";
+        long total = 0;
+
+        // Loop through each battery
+        for (int[] battery : batteryList) {
+            // Track values and positons of our findings
+            int[] values = new int[12];
+            int[] positions = new int[12];
+
+            for (int position = 0; position < 12; position++) {
+                // We start at the beginning or just after the last found position
+                int startPosition = (position == 0) ? 0 : positions[position - 1] + 1;
+                // We need to end early enough to fit the remaining digits
+                int endPosition = battery.length - (12 - position - 1); // Max value of position is 11
+
+                for (int cell = startPosition; cell < endPosition; cell++) {
+                    if (battery[cell] > values[position]) {
+                        values[position] = battery[cell];
+                        positions[position] = cell;
+                    }
+                }
+            }
+
+            // Process the int array into something useful
+            String resultString = intArrayToString(values);
+            long resultNumber = Long.parseLong(resultString);
+
+            // Add to the total
+            total += resultNumber;
+
+            if (DEBUG) {
+                System.out.printf("For battery: %s, max values are %s%n", intArrayToString(battery), resultString);
+            }
+        }
+
+        return Long.toString(total);
     }
 
     /*
      * *****************************************************************************
      * Other methods
      */
+
+    /**
+     * Convert an int array (the battery list) to a string without separators.
+     * 
+     * @param array
+     * @return string representation of the array
+     */
+    private String intArrayToString(int[] array) {
+        return Arrays.stream(array)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
+    }
 
     /**
      * *****************************************************************************
